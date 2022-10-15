54fc2807b6b7c5342bca55b440cd946d8181fd56
Sylaman <Sylaman.snake@gmail.com>
2022-10-15 19:28:28 +0300

    IntsCalculator developed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Ints calculator = new IntsCalculator();
+        System.out.println(calculator.sum(2, 2));
+        System.out.println(calculator.sum(10, 22));
+        System.out.println(calculator.mult(3, 10));
+        System.out.println(calculator.pow(2, 10));
     }
 }
